55cc4232d97be880217dd141e3a08515c10337f1
Leon Salenbacher <leon@salenbacher.com>
2023-03-26 21:33:31 +0200

    delete old portfolio

--- portfolio/scripts/project.js
+++
@@ -1,8 +0,0 @@
-class Project{
-    constructor(name, img_path, url, index){
-        this.name = name
-        this.img_path = img_path
-        this.url = url
-        this.index = index
-    }    
-}
